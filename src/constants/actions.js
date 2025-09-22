8eeb16ae98d963dee92167c8ab4fdfa3e99fc89b
Imandi Sai Ganesh <128854913+SaiGane5@users.noreply.github.com>
2025-09-22 23:51:24 +0530

    actions change for w2

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -2,42 +2,42 @@
 export const DAILY_ACTIONS = [
   { 
     id: 'a1', 
-    text: 'Generated SINGLE USE PLASTIC waste',
+    text: 'Segregated the fruits waste/egg shell before throwing the food in the dustbin in mess',
     icon: ''
   },
   { 
     id: 'a2', 
-    text: 'Paper Waste (stationary waste)',
+    text: 'Shared my home-brought snacks with friends so they would be eaten while fresh.',
     icon: ''
   },
   { 
     id: 'a3', 
-    text: 'Drink tea/coffee from a reusable cup',
+    text: 'Offered a prayer for the farmers and mess staff before eating',
     icon: ''
   },
   { 
     id: 'a4', 
-    text: 'Food Waste (mess/canteen)',
+    text: 'Planned a group order at Zaitoon/HFC/Swiggy with friends to ensure every dish would be finished',
     icon: ''
   },
   { 
     id: 'a5', 
-    text: 'Segregation of waste in HOSTEL',
+    text: 'Shared biscuits from an already opened packet with friends before they stale',
     icon: ''
   },
   { 
     id: 'a6', 
-    text: 'Use reusable shopping bags when you go out.',
+    text: 'Talked with friends while eating',
     icon: ''
   },
   { 
     id: 'a7', 
-    text: 'Consumed any fast food from vending machine',
+    text: 'Didn\'t take extra food the first time to avoid going to the counter again.',
     icon: ''
   },
   { 
     id: 'a8', 
-    text: 'Glass/Medicinal/Cosmetic/Hazardous waste',
+    text: 'Segregated food waste in the hostel into dry and wet waste whenever applicable',
     icon: ''
   },
 ];
